4c713032f06aac463ac28a88e7c07968a1700bca
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2015-04-29 10:39:09 +0700

    update provision shell

--- provision/go.root.sh
+++ provision/go.root.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-echo "go.root: runnning..."
+if [ -x /usr/local/go/ ];then 
+    echo "go.root: golang already installed"
+else
+    echo "go.root: runnning..." 
+    echo "go.root: export required env variable"
+    export GO_FILE_NAME=go1.4.2.linux-amd64.tar.gz
+    export HOME=/home/vagrant
 
-echo "go.root: export required env variable"
-export GO_FILE_NAME=go1.4.2.linux-amd64.tar.gz
-export HOME=/home/vagrant
+    echo "go.root: installing the latest stable release of golang"
+    mkdir -p /home/vagrant/provision
+    cd /home/vagrant/provision
+    wget --no-check-certificate --no-verbose https://storage.googleapis.com/golang/$GO_FILE_NAME
+    tar -C /usr/local -xzf $GO_FILE_NAME
 
-echo "go.root: installing the latest stable release of golang"
-mkdir -p /home/vagrant/provision
-cd /home/vagrant/provision
-wget --no-check-certificate --no-verbose https://storage.googleapis.com/golang/$GO_FILE_NAME
-tar -C /usr/local -xzf $GO_FILE_NAME
-
-echo "go.root: setup golang workspace"
-mkdir -p $HOME/go/src/github.com/pyk
-chown -R vagrant $HOME/go
+    echo "go.root: setup golang workspace"
+    mkdir -p $HOME/go/src/github.com/pyk
+    chown -R vagrant $HOME/go 
+fi
--- provision/node.root.sh
+++ provision/node.root.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
- 
-echo "node.root: installing the latest stable release of nodejs"
-mkdir /home/vagrant/provision
-git clone https://github.com/tj/n.git /home/vagrant/provision/n
-cd /home/vagrant/provision/n/
-make install
-n stable
+
+if [ -x /usr/local/bin/n ];then 
+    echo "node.root: node `node -v` already installed"
+else
+    echo "node.root: installing the latest stable release of nodejs"
+    mkdir /home/vagrant/provision
+    git clone https://github.com/tj/n.git /home/vagrant/provision/n
+    cd /home/vagrant/provision/n/
+    make install
+    n stable
+fi
